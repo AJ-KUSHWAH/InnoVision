2f96f595399229734c68a60263b0a42851324d7b
AJ-KUSHWAH <kushwahaj2707@gmail.com>
2024-10-26 22:37:49 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import ButtonGradiant from "./assets/svg/ButtonGradient";
+
+function App() {
+  return (
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+      <ButtonGradiant />
+    </>
+  );
+}
+
+export default App;
